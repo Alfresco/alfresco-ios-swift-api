01172c009b0f981c53bfcff763036df445c774d8
Florin Baincescu <florin.baincescu@ness.com>
2019-10-02 16:23:38 +0300

    MOBILESDK-214 - Extend current interface to support additional authentication with PKCE support

--- AlfrescoAuth/Sources/AlfrescoAuth.swift
+++ AlfrescoAuth/Sources/AlfrescoAuth.swift
@@ -9,7 +9,6 @@
 import UIKit
 import AlfrescoCore
 
-
 public enum AuthViewControllerType {
     case web
 }
--- AlfrescoAuth/Sources/Model/AlfrescoAuthSession.swift
+++ AlfrescoAuth/Sources/Model/AlfrescoAuthSession.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import AppAuth
 
 public struct AlfrescoAuthSession {
     var authorizationFlow: OIDExternalUserAgentSession?
