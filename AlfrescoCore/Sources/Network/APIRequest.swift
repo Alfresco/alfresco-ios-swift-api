4abc86188f1bfc856b4180b89d1961229b84c231
Emanuel Lupu-Marinei <emanuel.lupu@icloud.com>
2019-10-15 15:48:53 +0300

    IOS-1409 - Update documentation for Core module

--- AlfrescoCore/Sources/Network/APIRequest.swift
+++ AlfrescoCore/Sources/Network/APIRequest.swift
@@ -21,11 +21,22 @@ public enum ContentType: String {
     case json = "application/json"
 }
 
+
+/// Protocol declaration for request objects
 public protocol APIRequest: Encodable {
+    /**
+     Object type for the expected response.
+    - Remark: The specified type will be used in the  JSON transformation of the request response. Implementers of this protocol must provide
+     property decoders to support obect mapping.
+     */
     associatedtype Response: Decodable
     
+    // Path component of the request to be appended to the base url
     var path: String { get }
+    // HTTP verb of the request
     var method: HttpMethod { get }
+    // Request header parameters
     var headers: [String: ContentType] { get }
+    // Additional request parameters
     var parameters: [String: String] { get }
 }
