df7610b6bf64b3c48dab0acd56304c4a0f2b75a1
Emanuel Lupu-Marinei <emanuel.lupu@ness.com>
2019-10-15 11:58:02 +0300

    IOS-1407 - Update Unit Tests in Core module
    - solved findings

--- AlfrescoCore/AlfrescoCoreTests/TestData.swift
+++ AlfrescoCore/AlfrescoCoreTests/TestData.swift
@@ -9,16 +9,6 @@
 import Foundation
 @testable import AlfrescoCore
 
-//struct TestData {
-//    static let baseURLString = "http://alfresco-identity-service.mobile.dev.alfresco.me"
-//    static let path = "/auth"
-//    static let headerFieldsContentType = "Content-Type"
-//    static let headerFieldsContentTypeValue = "application/x-www-form-urlencoded"
-//    static let params = ["grant_type": "password"]
-//    static let paramsPercentEscaped = "grant_type=password"
-//    static let getURLWithParams = "http://alfresco-identity-service.mobile.dev.alfresco.me/auth?grant_type=password"
-//}
-
 struct TestData {
     static let baseURL = "http://domain.extension"
     static let invalidBaseURL = " "
