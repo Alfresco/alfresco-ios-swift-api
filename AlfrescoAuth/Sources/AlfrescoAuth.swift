e7255c1c55dc9407637e9ca147e42690c25456ac
Emanuel Lupu-Marinei <emanuel.lupu@ness.com>
2019-12-13 17:56:27 +0200

    IOS-1450 - AIMS Logout Support
    - Refactored AIMS logout method to use AppAuth unreleased logout support
    - Removed former logout method
    - Removed old tests
    - updated podspec version
    - updated AlfrescoCore pod version

--- AlfrescoAuth/Sources/AlfrescoAuth.swift
+++ AlfrescoAuth/Sources/AlfrescoAuth.swift
@@ -43,7 +43,6 @@ public struct AlfrescoAuth {
     var basicPresenter: AuthBasicPresenter?
     var refreshPresenter: RefreshTokenPresenter?
     var pkcePresenter: AuthPkcePresenter?
-    var logoutPresenter: LogoutPresenter?
     
     // Configuration
     var configuration: AuthConfiguration
@@ -113,8 +112,8 @@ public struct AlfrescoAuth {
     
     /** Given a path component,  it returns via a closure the available authentication type for the base URL defined in the AlfrescoConfiguration
      object and the defined path. If the path is not defined, then only the base URL will be used as reference.
-     - Parameter issuer: URL path for which the authentication type check is performed
-     - Parameter handler: Closure delivering updates on the authentication type. Possible values are base auth, AIMS and an optional error
+    - Parameter issuer: URL path for which the authentication type check is performed
+    - Parameter handler: Closure delivering updates on the authentication type. Possible values are base auth, AIMS and an optional error
      */
     public mutating func availableAuthType(for issuer: String = "", handler:@escaping ((Result<AvailableAuthType, APIError>) -> Void)) {
         if pkcePresenter == nil {
@@ -127,7 +126,7 @@ public struct AlfrescoAuth {
     }
     
     /** Designated safe session refresh method with Identity Service  using PKCE protocol.
-    - Parameter alfrescoAuthDelegate: Delegate used to report the state and response of the re-authentication request
+    - Parameter delegate: Delegate used to report the state and response of the re-authentication request
     - Parameter session: Valid session object that was created as a result of a successfull PKCE authentication.
      */
     public mutating func pkceRefresh(session: AlfrescoAuthSession, delegate: AlfrescoAuthDelegate) {
@@ -142,19 +141,24 @@ public struct AlfrescoAuth {
     
     
     /** Logs out the user  by expiring the refresh token for the current auth configuration.
-     - Remark: the access token remains valid for it's designated lifespan, but the next time a request to refresh the session it is received
-               the server will refuse and return a 401 Unauthorised response.
-     - Parameter delegate: Delegate used to report the state of the logout request.
+    - Remark: the access token remains valid for it's designated lifespan, but the next time a request to refresh the session it is received
+              the server will refuse and return a 401 Unauthorised response. Starting with iOS 12 and up, cookies are no longer reliably
+              shared with developers and as a workaround the logout request will take place under the same session they have been created
+              in the first place. To that end, a ASWebAuthenticationSession object will be used to run the logout request, thus deleting the remaining cookies.
+    - Parameter viewController: View controller on which to present the logout web view component
+    - Parameter delegate: Delegate used to report the state of the logout request.
+    - Parameter credential: Alfresco credential for which the logout is requested
      */
-    public mutating func logout(delegate: AlfrescoAuthDelegate) {
-        if pkcePresenter != nil {
-            // Invalidate any ongoing session
-            pkcePresenter?.authSession = nil
+    public mutating func logout(onViewController viewController:UIViewController, delegate: AlfrescoAuthDelegate, forCredential credential: AlfrescoCredential) {
+        if pkcePresenter == nil {
+            pkcePresenter = AuthPkcePresenter(configuration: configuration)
+            let authSession = AlfrescoAuthSession()
+            pkcePresenter?.authSession = authSession
         }
         
-        logoutPresenter = LogoutPresenter(configuration: configuration)
-        logoutPresenter?.authDelegate = delegate
-        logoutPresenter?.execute()
+        pkcePresenter?.presentingViewController = viewController
+        pkcePresenter?.authDelegate = delegate
+        pkcePresenter?.logout(forCredential: credential)
     }
 }
 
